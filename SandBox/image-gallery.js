7304970d6aa9481a84a81c382df1b87ed89cc8a6
Danielle-Morvant <daniellemorvant1@gmail.com>
2025-06-14 04:02:31 -0500

    working

--- SandBox/image-gallery.js
+++ SandBox/image-gallery.js
@@ -1,49 +1,29 @@
- const gallery = document.getElementById("gallery");
+const gallery = document.getElementById("gallery");
 
+  // Fetch 12 random images from Lorem Picsum
+async function fetchImages() {
+  try {
 
+    // CONNECTS TO THE WEBSITE AND STORES THE RESULTS IN RES
+    const res = await fetch("https://picsum.photos/v2/list?page=2&limit=50");
 
- // Fetch 12 random images from Lorem Picsum
+    //CONVERTS THE RESULTS FROM STRING FORM TO OBJECT OR DATA FORM
+    const data = await res.json();
 
- async function fetchImages() {
+    // CONVERTS THE DATA TO HTML AND INSERTS IT INTO THE gallery ID TAG 
+    gallery.innerHTML = data.map((image) => {
+        return ` 
+          <div>
+            <img src="${image.download_url}" alt="Random image by ${image.author}" />
+          </div>`;
+      })
 
- try {
+      .join("");
+  } catch (error) {
+    //IF THERES AN ERROR, RUN THIS CODE
+    console.error("Failed to fetch images:", error);
+  }
+}
 
- const res = await fetch(
-
- "https://picsum.photos/v2/list?page=2&limit=50"
-
- );
-
- const data = await res.json();
-
-
-
- gallery.innerHTML = data
-
- .map((image) => {
-
- return `
-
- <div>
-
- <img src="${image.download_url}" alt="Random image by ${image.author}" />
-
- </div>
-
- `;
-
- })
-
- .join("");
-
- } catch (error) {
-
- console.error("Failed to fetch images:", error);
-
- }
-
- }
-
-
-
- fetchImages();
+//ACTIVATE THE FUNCTION, FUNCTIONS DON'T RUN UNTIL THY'RE ACTIVATED
+fetchImages();
