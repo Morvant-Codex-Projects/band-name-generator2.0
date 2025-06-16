07f98a5f0df09e3dae3ed59a8e5f6081127f5fda
Danielle-Morvant <daniellemorvant1@gmail.com>
2025-06-16 12:45:11 -0500

    troubbleshooting

--- navbar.js
+++ navbar.js
@@ -2,7 +2,7 @@
       <a href="index.html" class="bg-red-600">Home</a>
       <a href="pokemon.html" class="bg-red-600">Pokemon</a>
       <a href="Image-Gallery.html" class="bg-red-600">Image Gallery</a>
-      <a href="flexbox.html"class="bg-red-600" >Flexbox</a>
+      <a href="flexbox.html" class="bg-red-600">Flexbox</a>
        <a href="tailwindFlexbox.html" class="bg-red-600">Tailwind</a>
   </nav>`;
 
@@ -11,4 +11,4 @@ const header = document.getElementById("header");
 header.innerHTML = navbar;
 
 // INSERT THE NAVBAR INTO THE HEADER
-header. innerHTML = header. innerHTML + navbar;
+header.innerHTML = navbar;
